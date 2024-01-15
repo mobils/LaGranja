55bf3393d1ad787f2676807c22fc7cf5bc1809dd
Eva <egutierrez@educem.com>
2024-01-15 11:36:53 +0100

    upgraded AGP 7.4.1

--- app/src/main/java/jocs/eva/cat/laGranja/PianoFragment.java
+++ app/src/main/java/jocs/eva/cat/laGranja/PianoFragment.java
@@ -1,19 +1,25 @@
 package jocs.eva.cat.laGranja;
 
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link PianoFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PianoFragment extends Fragment {
+public class PianoFragment extends Fragment implements  View.OnTouchListener, View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +29,8 @@ public class PianoFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    Button bt_do, bt_re;
+
 
     public PianoFragment() {
         // Required empty public constructor
@@ -59,11 +67,55 @@ public class PianoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_piano, container, false);
+        View v= inflater.inflate(R.layout.fragment_piano, container, false);
+        bt_do =v.findViewById(R.id.vw_bt_do);
+        bt_re =v.findViewById(R.id.vw_bt_re);
 
+        //bt_do.setOnHoverListener(this); //No funciona en pantalles de mòbils.
+        bt_do.setOnClickListener(this);
+        bt_re.setOnClickListener(this);
+        bt_re.setOnTouchListener(this);
+
+        return v;
         //TODO: Exercici 4: Ja tens creat un disseny de piano, importa els fitxers dels sons i fes que soni
         // Has d'inflar el fragment on vulguis, o al MainActivity o en un altre.
         // Decideix si vols que les tecles del piano sonin quan cliques o quan passes per sobre (onHoverListener)
 
+    }
+
+    @Override
+    public boolean onHover(View v, MotionEvent event) {
+        /*Només un animation per view, si es volen fer més, afegirles al xml dins un <selector>*/
+
+        Animation animation = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.rota);
+
+        //v.startAnimation(animation);
+        //bt_do.startAnimation(animation);
+        return false;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Animation animation = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.rota);
+        MediaPlayer so = null;
+        switch (view.getId()) {
+            case R.id.vw_bt_do:
+                so = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.notac); break;
+            case R.id.vw_bt_re:
+                so = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.notad); break;
+                //view.startAnimation(animation);
+            default:
+                so = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.notad); break;
+        }
+        so.start();
+    }
+
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        Animation animation = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.rota);
+
+        view.startAnimation(animation);
+        return false;
     }
 }
